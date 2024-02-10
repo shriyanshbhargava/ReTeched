f80e6b9adc3ef49ffd75e38e609b84ec39ab9606
Shriyansh Bhargava <63575417+shriyanshbhargava@users.noreply.github.com>
2024-02-10 18:10:56 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import { Home } from "./Pages/Home";
-import Footer from "./Components/Footer/Footer"
-
-
+import Footer from "./Components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Locate } from "./Pages/Locate";
 
 function App() {
   return (
     <div className="App bg-white-50">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/locate" component={<Locate />} />
+          {/* Add more routes as needed */}
+        </Routes>
+        <Header />
+        <Home />
+        <Footer />
+      </Router>
     </div>
   );
 }
